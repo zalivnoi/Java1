f8d96a63753367c88fc2243af35e434a3a22b62f
zalivnoi <dlatankov91@bk.ru>
2022-05-28 19:25:44 +0300

    add homework 2

---
+++ HomeworkGB/src/main/java/Homework2/Homework2.java
@@ -0,0 +1,20 @@
+package Homework2;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        summInPredel();
+    }
+    public static void summInPredel(){
+        int a = 25;
+        int b = 8;
+        boolean c = true;
+        if ((a + b) > 10 && (a + b) < 20) {
+            System.out.println(c);
+        }
+        else {
+            c = false;
+            System.out.println(c);
+        }
+
+    }
+}
